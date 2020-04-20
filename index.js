7c4542c46ceb94e7bc83f41fa24747488d1d14a4
Anovo <Ahmed.ashraff@live.com>
2020-04-20 22:54:13 +0200

    Sign In

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
-const app  = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.listen(PORT , () => console.log(`Server started on port ${PORT}`));
+app.use(express.urlencoded({ extended: false }));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 //DB connection
 require("./database/connection");
 //Uers API route
