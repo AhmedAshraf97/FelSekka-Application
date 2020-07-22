5e0834d3f8ba52e130c8afdc65a60da2ec53a682
NarimanReda <47919535+NarimanReda@users.noreply.github.com>
2020-07-22 19:55:55 +0200

    ''

--- routes/api/add_car.js
+++ routes/api/add_car.js
@@ -135,11 +135,10 @@ router.post('/', async(req, res) => {
         }).then(user => {
             if (user) {
                 var pythonresult;
-                var carlicense =JSON.parse(req.body.carlicensefront) ;
                 const python = spawn('python', ["../../authentication.py",
                 req.body.plateletters,
                 req.body.platenumbers,
-                carlicense
+                req.body.carlicensefront
                 ]);
                 python.stdout.on('data', function (data) {
                 pythonresult=data.toString();
